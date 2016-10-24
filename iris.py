150e644384040cb94e9cda4712d2802c99776f4d
John Krieger <Johnkkrieger@gmail.com>
2016-10-23 19:03:30 -0600

    Hopeful the second part of the assignment

--- iris.py
+++ iris.py
@@ -1,29 +1,70 @@
 import nodeClass
 from sklearn import datasets
+import math
 import random
 
 iris = datasets.load_iris()
 
 nodes = [nodeClass.Node() for count in xrange(5)]
 
-nodes[0].weights = [1.1,2.1,0.7,0.8,2.0]
-nodes[1].weights = [1.1,2.1,0.7,0.8,2.0]
-nodes[2].weights = [1.1,2.1,0.7,0.8,2.0]
-nodes[3].weights = [1.1,2.1,0.7,0.8,2.0]
-nodes[4].weights = [1.1,2.1,0.7,0.8,2.0]
+
 
 irisD = iris.data.tolist()
 
-nodes[1].attribute = irisD[0][0]
-nodes[2].attribute = irisD[0][1]
-nodes[3].attribute = irisD[0][2]
-nodes[4].attribute = irisD[0][3]
 
-nodes[0].value = -1
-nodes[1].value = 0
-nodes[2].value = 1
-nodes[3].value = 0
-nodes[4].value = 1
 
-for x in xrange(5):
-    print nodes[x].value
+#First Layer
+
+#Weights are assigned
+nodes[0].weights = [0.2,-0.3,0.4,-0.9,0.7]
+nodes[1].weights = [0.3,-0.5,0.7,0.6,-0.6]
+
+#Values are assigned
+nodes[0].attribute = [-1] + irisD[0]
+nodes[1].attribute = [-1] + irisD[0] # Add Bias to front of values
+
+nodes[0].value = nodes[0].math1()
+
+
+nodes[1].value = nodes[1].math1()
+
+nodes[0].activation = nodes[0].activator(nodes[0].value)
+
+nodes[1].activation = nodes[1].activator(nodes[1].value)
+
+#Second Layer
+nodes[2]
+nodes[3]
+nodes[4]
+
+
+nodes[2].weights = [-0.9,0.7,0.6,-0.6]
+nodes[3].weights = [0.3,-0.5,0.2,-0.3]
+nodes[4].weights = [0.7,0.6,0.4,-0.9]
+
+nodes[2].attribute = [-1] + [nodes[0].value] + [nodes[1].value]
+nodes[3].attribute = [-1] + [nodes[0].value] + [nodes[1].value]
+nodes[4].attribute = [-1] + [nodes[0].value] + [nodes[1].value]
+
+nodes[2].value = nodes[2].math1()
+nodes[3].value = nodes[3].math1()
+nodes[4].value = nodes[4].math1()
+
+nodes[2].activation = nodes[2].activator(nodes[2].value)
+nodes[3].activation = nodes[3].activator(nodes[3].value)
+nodes[4].activation = nodes[4].activator(nodes[4].value)
+
+if nodes[2].value < nodes[2].activation:
+    print 0
+else:
+    print 1
+
+if nodes[3].value < nodes[3].activation:
+    print 0
+else:
+    print 1
+
+if nodes[4].value < nodes[4].activation:
+    print 0
+else:
+    print 1
--- nodeClass.py
+++ nodeClass.py
@@ -1,14 +1,29 @@
+import math
 class Node:
 
     value = 0
     numOfWeights = 0
     layers = 0
     weights = []
-    attribute = 0
+    attribute = []
+    bias = -1
+    activation = 0
 
-    def __int__(self, value, numOfWeights, layers, weights,attribute):
+    def __int__(self, value, numOfWeights, layers, weights,attribute,bias,activation):
         self.value = value
         self.numOfWeights = numOfWeights
         self.layers = layers
         self.weights = weights
         self.attribute = attribute
+        self.bias = bias
+        self.activation = activation
+
+    def math1(self):
+        answer = [weights*attribute for weights,attribute in zip(self.weights,self.attribute)]
+        ranswer = sum(answer)
+        return ranswer
+
+
+    def activator(self,value):
+        x = value * -1
+        return 1 / (1 + math.pow(math.e, x))
